3b779bfe43b1ba8aaab898e31a3ac66914b2c6be
codedbykaif <kaif_246048@saitm.ac.in>
2025-06-21 14:01:18 +0530

    Add files via upload

---
+++ LOOPs/greetingntimes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+  // printing hello n times taking n as an input
+  int x;
+  cout<<"Enter the number : ";
+  cin>>x;
+  for(int i=1; i<=x; i++){
+    cout<<"Hello"<<endl;
+  }
+  return 0;
+}
