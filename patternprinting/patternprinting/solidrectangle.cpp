e592d2564ec54194bcf03ff12a657841c7102ceb
codedbykaif <kaif_246048@saitm.ac.in>
2025-07-21 14:23:31 +0530

    Add files via upload

---
+++ patternprinting/patternprinting/solidrectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"Enter number of rows :";
+  cin>>n;
+  for(int i=1; i<=n; i++){
+    cout<<"*****"<<endl;
+  }
+
+  return 0;
+}
