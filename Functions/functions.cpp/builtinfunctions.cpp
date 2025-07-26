89a3afb48d961e9637be21f441fe3c26436efff8
codedbykaif <kaif_246048@saitm.ac.in>
2025-07-26 14:58:22 +0530

    Add files via upload

---
+++ Functions/functions.cpp/builtinfunctions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+int x,y;
+cin>>x>>y;
+cout<<"the minimum number is : "<<min(x,y);
+
+// int a,c;
+// cin>>a,b;
+// cout<<"The maximum number is : "<<max(a,b);
+  return 0;
+}
---
+++ Functions/functions.cpp/printmaxusingfunction.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+// int x,y;
+// cin>>x>>y;
+// cout<<"the maximum number is : "<<max(x,y);
+cout<<sqrt(3);
+// int a,c;
+// cin>>a,b;
+// cout<<"The maximum number is : "<<max(a,b);
+  return 0;
+}
