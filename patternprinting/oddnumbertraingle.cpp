8b57f41798781429c036a0c47352e69a821c94f1
codedbykaif <kaif_246048@saitm.ac.in>
2025-07-26 09:50:34 +0530

    Add files via upload

--- patternprinting/oddnumbertraingle.cpp
+++ patternprinting/oddnumbertraingle.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 int main(){
   int n;
-  cout<<"Enter the number of rows: ";
-   cin>>n;
-   for(int i=1; i<=n; i++){
-    for(int j=1; j<=n+1-i; j++){
-      cout<<j;
-    }
-    cout<<endl;
-   }
+  cout<<"Enter number of rows: ";
+  cin>>n;
+  for(int i=1; i<=n; i++){
+  int a=1;
+  for(int j=1; j<=i; j++){
+    cout<<a;
+    a+=2;
+  }
+  cout<<endl;
+  }
+
+  
   return 0;
 }                                                                                                                                                                                                                                                                                                                                                                                    
